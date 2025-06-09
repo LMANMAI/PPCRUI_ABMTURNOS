ee7d9b61e776a7314610af12c1142d39c33cda26
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2025-06-09 20:32:37 -0300

    chore: login

--- containers/Login/index.tsx
+++ containers/Login/index.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Container, Box, Stack, Field, Input, Button } from "@chakra-ui/react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Container display={"flex"} justifyContent={"center"} alignItems={"center"}>
+      <Box backgroundColor={"white"} padding={10} width={500} borderRadius={10}>
+        <Stack color={"black"} my={2}>
+          <Field.Root required>
+            <Field.Label>
+              Email <Field.RequiredIndicator />
+            </Field.Label>
+            <Input placeholder="Enter your email" />
+            <Field.HelperText>We'll never share your email.</Field.HelperText>
+          </Field.Root>
+        </Stack>
+        <Stack color={"black"} my={2}>
+          <Field.Root required>
+            <Field.Label>
+              Email <Field.RequiredIndicator />
+            </Field.Label>
+            <Input placeholder="Enter your email" />
+            <Field.HelperText>We'll never share your email.</Field.HelperText>
+          </Field.Root>
+        </Stack>
+        <Button variant={"solid"} my={2}>
+          Ingresar
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default LoginPage;
