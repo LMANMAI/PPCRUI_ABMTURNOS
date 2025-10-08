541b8f9f658c743be21d563e53cda82a79da105a
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2025-10-08 11:24:00 -0300

    chore: ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
