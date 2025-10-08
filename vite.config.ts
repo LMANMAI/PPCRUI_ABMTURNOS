4010c15761f9058946f007b22f0206cb7f6fdc22
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2025-10-08 11:30:36 -0300

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "."),
+      "@src": path.resolve(__dirname, "src"),
     },
   },
 });
